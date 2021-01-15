45111fa2300221db32a1794f134e4183514467b7
dheera <dheera@dheera.net>
2021-01-15 09:58:36 -0800

    code

--- code.py
+++ code.py
@@ -7,16 +7,16 @@ import os
 import time
 import microcontroller
 
-LOAD_FREEDOM = True
-LOAD_DISPLAY = True    # https://www.adafruit.com/product/4440
-LOAD_WATCHDOG = True
-LOAD_BME680 = True     # https://www.adafruit.com/product/3660
-LOAD_SGP30 = True      # https://www.adafruit.com/product/3709
-LOAD_SCD30 = True      # https://www.seeedstudio.com/Grove-CO2-Temperature-Humidity-Sensor-SCD30-p-2911.html
-LOAD_MCGASV2 = True    # https://www.seeedstudio.com/Grove-Multichannel-Gas-Sensor-v2-p-4569.html
-LOAD_SEN0321 = True    # https://www.dfrobot.com/product-2005.html
-LOAD_BNO08X = True     # https://www.adafruit.com/product/4754
-LOAD_PMSA003I = True   # https://www.adafruit.com/product/4632
+LOAD_FREEDOM = True    # syncs data to device on freedomrobotics.ai; requires credentials.json with freedom device credentials
+LOAD_WATCHDOG = True   # initiates a watchdog so the uc resets if any blocking call hangs for more than 30 seconds
+LOAD_DISPLAY = True    # 128x32 OLED display: https://www.adafruit.com/product/4440
+LOAD_BME680 = True     # pressure, humidity, temperature, voc: https://www.adafruit.com/product/3660
+LOAD_SGP30 = True      # voc (better than bme680): https://www.adafruit.com/product/3709
+LOAD_SCD30 = True      # co2, humidity, temp: https://www.seeedstudio.com/Grove-CO2-Temperature-Humidity-Sensor-SCD30-p-2911.html
+LOAD_MCGASV2 = True    # multi-channel gas sensor: https://www.seeedstudio.com/Grove-Multichannel-Gas-Sensor-v2-p-4569.html
+LOAD_SEN0321 = True    # ozone sensor: https://www.dfrobot.com/product-2005.html
+LOAD_BNO08X = True     # imu: https://www.adafruit.com/product/4754
+LOAD_PMSA003I = True   # air quality particulate matter sensor: https://www.adafruit.com/product/4632
 
 if LOAD_WATCHDOG:
     from watchdog import WatchDogMode
