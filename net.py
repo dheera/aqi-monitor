11753001a14660cba4ae214b9e9bc7765c40e9a9
dheera <dheera@dheera.net>
2021-01-15 09:25:56 -0800

    cleanup net.py

--- net.py
+++ net.py
@@ -1,13 +1,12 @@
-import ipaddress
-import ssl
-import wifi
-import socketpool
 import adafruit_requests
- 
-# URLs to fetch from
-TEXT_URL = "http://wifitest.adafruit.com/testwifi/index.html"
-JSON_QUOTES_URL = "https://www.adafruit.com/api/quotes.php"
-JSON_STARS_URL = "https://api.github.com/repos/adafruit/circuitpython"
+import ipaddress
+import json
+import rtc
+import socketpool
+import ssl
+import time
+import wifi
+
  
 # Get wifi details and more from a secrets.py file
 try:
@@ -37,10 +36,6 @@ print("My IP address is", wifi.radio.ipv4_address)
 pool = socketpool.SocketPool(wifi.radio)
 requests = adafruit_requests.Session(pool, ssl.create_default_context())
 
-import json
-import rtc
-import time
-
 print("Setting system time")
 
 response = requests.get("http://worldtimeapi.org/api/timezone/Etc/UTC")
@@ -51,24 +46,3 @@ if response.status_code == 200:
 else:
     print("Setting time failed")
 
-#print("Fetching text from", TEXT_URL)
-#response = requests.get(TEXT_URL)
-#print("-" * 40)
-#print(response.text)
-#print("-" * 40)
- 
-#print("Fetching json from", JSON_QUOTES_URL)
-#response = requests.get(JSON_QUOTES_URL)
-#print("-" * 40)
-#print(response.json())
-#print("-" * 40)
- 
-#print()
- 
-#print("Fetching and parsing json from", JSON_STARS_URL)
-#response = requests.get(JSON_STARS_URL)
-#print("-" * 40)
-#print("CircuitPython GitHub Stars", response.json()["stargazers_count"])
-#print("-" * 40)
- 
-#print("done")
